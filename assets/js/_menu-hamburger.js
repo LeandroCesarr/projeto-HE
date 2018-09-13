4b9f36d4b0cf33367097aa88993ffa1b153680cc
Leandro C. Silva <leandrocesar49@hotmail.com>
2018-09-12 23:56:48 -0300

    Add component and modified about.html

---
+++ assets/js/_menu-hamburger.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+	var element = document.getElementById("navMenu");
+	   element.classList.toggle("is-active");
+
+	var element2 = document.getElementById("navBox");
+	element2.classList.toggle("about-nav__active");
+}
