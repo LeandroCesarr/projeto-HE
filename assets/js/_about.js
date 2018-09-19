30839298288591c49285c471f9566e14b51398fd
Leandro C. Silva <leandrocesar49@hotmail.com>
2018-09-18 22:30:54 -0300

    Add jump.js Funcional

---
+++ assets/js/_about.js
@@ -0,0 +1,52 @@
+(function() {
+
+  var navMenuButton = document.getElementById('nav-menu-button');
+  var navMenu = document.getElementById('navBox');
+
+  function construct() {
+    setupNavMenu();
+    setupNavMenuLinks();
+  }
+
+  function setupNavMenu() {
+    navMenuButton.addEventListener('click', function() {
+      toggleMenu();
+    });
+  }
+
+  function toggleMenu() {
+    navMenuButton.classList.toggle("is-active");
+    navMenu.classList.toggle("about-nav__active");
+  }
+
+  function setupNavMenuLinks() {
+    var links = navMenu.querySelectorAll('a');
+
+    for (var i = 0; i < links.length; i++) {
+
+      links[i].addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var sectionId = event.currentTarget.getAttribute('href');
+        // var offset = event.currentTarget.getAttribute('data-offset');
+
+        jumpToSection(sectionId);
+        toggleMenu();
+
+      });
+
+    }
+  }
+
+  function jumpToSection(section_id, offset) {
+    Jump(section_id, {
+      duration: 1000,
+      offset: -87,
+      a11y: false
+    });
+  }
+
+  construct();
+
+})();
+
--- assets/js/_menu-hamburger.js
+++
@@ -1,7 +0,0 @@
-function menuToggle() {
-	var element = document.getElementById("navMenu");
-	   element.classList.toggle("is-active");
-
-	var element2 = document.getElementById("navBox");
-	element2.classList.toggle("about-nav__active");
-}
