c153cff84e190a8898186cf60fd902df6508eaed
Leandro C. Silva <leandrocesar49@hotmail.com>
2018-11-08 23:48:16 -0200

    Add JQuery FODASE

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,3 +54,33 @@
 
 })();
 
+
+(function(){
+
+  var $target = [];
+      $target[0] = $('.s-summary--title').offset().top;
+      $target[1] = $('.s-team--title').offset().top;
+      $target[2] = $('.s-history--title').offset().top;
+      $target[3] = $('.s-projects--title').offset().top;
+
+  function changeScroll() {
+    var documentTop = $(document).scrollTop();
+
+      if ((documentTop > $target[1] - 20) && (documentTop < $target[2])) {
+        document.getElementsByTagName('h1')[0].innerHTML = "Equipe"
+      }
+      else if ((documentTop > $target[2] - 20) && (documentTop < $target[3] - 20)) {
+        document.getElementsByTagName('h1')[0].innerHTML = "História"
+      }
+      else if (documentTop > $target[3] - 20) {
+        document.getElementsByTagName('h1')[0].innerHTML = "Projetos"
+      }
+      else {
+        document.getElementsByTagName('h1')[0].innerHTML = "&lt PineappleDevs / &gt"
+      }
+
+    }
+  $(document).scroll(function(){changeScroll()});
+
+})();
+
