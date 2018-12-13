1e953e8265600e40c241b02d4671115f5f7fd215
Leandro C. Silva <leandrocesar49@hotmail.com>
2018-12-12 23:44:35 -0200

    Footer provisorio

---
+++ src/assets/js/components/backTop.js
@@ -0,0 +1,32 @@
+import Jump from 'jump.js';
+
+class BackTop {
+  constructor() {
+    this.elm = document.querySelector('[data-back]');
+    this.href = document.querySelector('[data-back]').getAttribute('href');
+    this.setup();
+  }
+
+  setup() {
+    console.log(this.elm);
+    this.elm.addEventListener('click', () => {
+      BackTop.backTopJump(this.href);
+    });
+  }
+
+  static backTopJump(pao) {
+    Jump(pao, {
+      duration: 1000,
+      offset: -87,
+      a11y: false,
+    });
+  }
+}
+
+export default {
+  create() {
+    return new BackTop();
+  },
+};
+
+export const Class = BackTop;
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,9 +2,11 @@
 import AOS from 'aos';
 import MenuPage from './components/menuPage';
 import typeWriter from './components/typeWriter';
+import BackTop from './components/backTop';
 
 MenuPage.create('nav-menu-button', 'navBox');
 typeWriter.create();
+BackTop.create('[data-back]');
 
 AOS.init({
   offset: 160,
